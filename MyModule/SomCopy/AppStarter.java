c164b24562f691f728bf99213b71f516c7b7f914
Qoofu <60098277+cateto@users.noreply.github.com>
2020-06-03 23:24:05 +0900

    Create AppStarter.java
    20/05/13

---
+++ MyModule/SomCopy/AppStarter.java
@@ -0,0 +1,11 @@
+class AppStarter
+{
+	public static void main(String[] args) 
+	{
+		new SomCopyIndex();
+		System.out.println("*****************************************");
+		System.out.println("사용해주셔서 감사합니다!");
+		System.out.println("(^_^)/ (^v^)/ (^x^)/");
+		System.out.println("*****************************************");
+	}
+}
